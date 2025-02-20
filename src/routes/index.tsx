3b7a6eeaf8bf3623f6fa122a9e97a4f5939147bb
nguyeninnov8 <rn100204@gmail.com>
2025-02-20 17:26:47 +0700

    refactor: reorganize authentication components and styles, add missing files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AdminRoutes from './AdminRoutes';
 import config from '@/config';
 import { ROLE } from '@/constants';
-
-// import AdminRoutes from './AdminRoutes';
-
-//* Layouts
 import MemberHeaderLayout from '@/layouts/Member/HeaderLayout';
 import MemeberSidebarLayout from '@/layouts/Member/SidebarLayout';
-import config from '@/config/routes';
-import ValidateEmail from '@/pages/auth/SignUp/ConfirmEmail/emailConfirm';
+import ValidateEmail from '@/pages/Auth/SignUp/ConfirmEmail/emailConfirm';
 import PublicLayout from '@/layouts/PublicLayout';
 import PrivateRoute from '@/components/Auth/PrivateRoutes';
 import AuthRoutes from './AuthRoutes';
@@ -18,8 +13,8 @@ import PersistToken from '@/components/Auth/PeristLogin';
 
 //* Lazy load pages
 const Home = lazy(() => import('@/pages/home'));
-const Login = lazy(() => import('@/pages/auth/Login'));
-const SignUp = lazy(() => import('@/pages/auth/SignUp'));
+const Login = lazy(() => import('@/pages/Auth/Login'));
+const SignUp = lazy(() => import('@/pages/Auth/SignUp'));
 
 const RouterComponent = () => {
   const router = createBrowserRouter([
